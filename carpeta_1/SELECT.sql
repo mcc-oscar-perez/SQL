7b5a2e9a8a523d768f71e65aa78ecd1ab19141fb
Thestaloz <oskr-75@hotmail.com>
2025-02-18 17:38:24 -0600

    agregamos SELECT a carpeta 1

---
+++ carpeta_1/SELECT.sql
@@ -0,0 +1,11 @@
+-- Ejercicio practico de SELECT numero 1
+-- SELECT practical exercise number 1
+ 
+SELECT * 
+FROM Actores
+
+-- Ejercicio practico de SELECT numero 2
+-- SELECT practical exercise number 2
+
+SELECT titulo, temporada
+FROM Episodios
