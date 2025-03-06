52c0265e6c9b5363ee1eba02c00d5824c3e11609
Thestaloz <oskr-75@hotmail.com>
2025-03-06 14:11:28 -0600

    agregamos modificaciones a CREATE_VIEW

--- carpeta_5/CREATE_VIEW.sql
+++ carpeta_5/CREATE_VIEW.sql
@@ -9,3 +9,13 @@ from empleados
 where depto_id = 1; 
 
 select * from visataempleadostecnologia
+
+-- 2
+
+create view VisataEmpleadosProyectos as
+select e.nombre, e.apellido, p.nombre as nombre_proyecto
+from empleados e
+join asignacionesdeproyectos a on a.empleado_id = e.empleado_id
+join proyectos p on a.proyecto_id = p.proyecto_id
+
+select * from VisataEmpleadosProyectos
